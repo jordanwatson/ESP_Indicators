6c29817f558aa268a4711b641b45d5755c90570a
Jordan Watson <jordanwatson@users.noreply.github.com>
2021-10-15 15:05:17 -0800

    Init commit

---
+++ ESP_Chlorophyll_Crab.R
@@ -0,0 +1,22 @@
+#  Calculate spring chlorophyll biomass for St Matts King Crab and for Bristol Bay King Crab.
+#  jordan.watson@noaa.gov
+
+
+library(tidyverse)
+library(lubridate)
+
+temp <- readRDS("Data/chlorophyll/modis_8day/merged/merged_8day_2003_2021_EBS.RDS") %>%
+  mutate(month=month(date),
+         YEAR=year(date)) %>%
+  filter(crab%in%c("StMatts","BristolBay") & month%in%c(4:6)) %>%
+  group_by(YEAR,crab) %>%
+  summarise(DATA_VALUE=mean(chlorophyll,na.rm=TRUE))
+
+lapply(unique(temp$crab),function(x) temp %>% 
+         filter(crab==x) %>% 
+         mutate(INDICATOR_NAME=case_when(crab=="BristolBay"~"SPR_CHLORO_BIOM_SEBS_IS",
+                                         crab=="StMatts"~"SPR_CHLORO_BIOM_SMBKC")) %>% 
+         dplyr::select(-crab) %>% 
+         write.csv(paste0("Data/Kalei/Spring_Chlorophylla_Biomass_",x,"_Satellite.csv"),row.names = F))
+
+
